0fabfb9e44338fdcb20cde155c6c9141c6c685c7
usernaimandrey <anshlyapnikov@yandex.ru>
2022-10-22 02:15:56 +0300

    fix migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_21_210559) do
+ActiveRecord::Schema.define(version: 2022_10_21_231444) do
 
   create_table "repositories", force: :cascade do |t|
     t.string "full_name"
